b91c3928a79041fab9ff58d69a25cac16c60d9f9
madhavareddyrb <madhavareddyrb@gmail.com>
2025-09-13 22:05:56 +0530

    completed

--- Todo/script.js
+++ Todo/script.js
@@ -4,8 +4,9 @@ const todoInput = document.getElementById("todo-input");
 const addTaskButton = document.getElementById("add-task-btn");
 const todoList = document.getElementById("todo-list");
 
-let tasks = JSON.parse(localStorage.getItem("tasks"))|| [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 tasks.forEach((task) => renderTask(task));
+
 addTaskButton.addEventListener("click", () => {
   const taskText = todoInput.value.trim();
   if (taskText == "") return;
@@ -16,17 +17,40 @@ addTaskButton.addEventListener("click", () => {
   };
   tasks.push(newTask);
   saveTasks();
+  renderTask(newTask);
   todoInput.value = "";
   console.log(tasks);
 });
 
-function saveTasks(){
-  localStorage.setItem("tasks",JSON.stringify(tasks));
-};
 
 function renderTask(task){
   console.log(task);
   console.log(task.text);
-  
+  const li = document.createElement("li");
+  li.setAttribute("data_id", task.id);
+  if (task.completed) li.classList.add('completed');
+  li.innerHTML = `
+  <span>${task.text} </span>
+  <button>delete</button>
+
+  `;
+  li.addEventListener('click',(e) => {
+    if (e.target.tagName === "BUTTON") return;
+    task.completed =! task.completed;
+    li.classList.toggle("completed");
+    saveTasks();
+
+  });
+  li.querySelector('button').addEventListener("click", (e) => {
+    e.stopPropagation();
+    tasks = tasks.filter((t) => t.id === task.id);
+    li.remove();
+    saveTasks();
+  });
+  todoList.appendChild(li);
 }
+
+function saveTasks(){
+  localStorage.setItem("tasks",JSON.stringify(tasks));
+};
 });
