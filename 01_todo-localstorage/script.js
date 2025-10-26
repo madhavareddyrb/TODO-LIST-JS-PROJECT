60dd43ad02d140b5f8c1f8ef7aab56ba04fdedd8
madhavareddyrb <madhavareddyrb@gmail.com>
2025-10-26 12:50:56 +0530

    Agaiin Tried On my Own and Completed The Project

--- 01_todo-localstorage/script.js
+++ 01_todo-localstorage/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const todoInput = document.getElementById("todo-input");
   const addTaskButton = document.getElementById("add-task-btn");
   const todoList = document.getElementById("todo-list");
-
+  
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
   tasks.forEach((task) => renderTask(task));
